a6ccc2b082921ba65df02136c6a6f6270086e518
jepoy92 <jeffrey1choi@gmail.com>
2020-11-17 18:50:23 -0800

    added animation back into landing page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import seeds from "../public/assets/images/seeds.png"
+import Typical from "react-typical"
 
 function Home(){
     return(
@@ -9,6 +10,13 @@ function Home(){
 
         <img id="seeds" class="m-auto text-center w-1/2 h-auto" src={ seeds } />
         <h1 id="seedsBottomText" class="animation text-green1 md:text-4xl font-bold focus:outline-none sm:text-2xl">start small. grow fast. be happy</h1>
+            
+        <h1>
+            <Typical
+            steps={['start small. grow fast. be happy', 2000,]}
+            wrapper="b"
+            />
+        </h1>
 
         <button id="login" class="block m-auto my-5 w-32 p-2 bg-lime1 text-white text-lg rounded-full font-bold focus:outline-none">
             Login
@@ -18,7 +26,7 @@ function Home(){
             Sign up
         </button>
 
-        // <script src="/public/assets/js/dist/index.js"></script>
+        <script src="/public/assets/js/dist/index.js"></script>
 
     </div>
     </div>
