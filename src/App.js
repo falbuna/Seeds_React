a0ebc8503c465d7b001fd78cad3a8c8c40be512b
Francis Albuna <falbuna1@gmail.com>
2020-11-17 19:49:31 -0800

    Added signup page.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import About from "./pages/About";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 import Navbar from "./components/Navbar";
 import './index.css';
 
@@ -15,6 +16,7 @@ function App() {
         <Route exact path="/home" component={Home} />
         <Route exact path="/about" component={About} />
         <Route exact path="/login" component={Login} />
+        <Route exact path="/Signup" component={Signup} />
     </div>
     </Router>
   );
