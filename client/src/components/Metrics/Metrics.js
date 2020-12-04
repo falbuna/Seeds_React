4d7a7f2fa19cced827244a0f57b05fd18a579b33
Matthew Milici <milici.m915@gmail.com>
2020-12-03 20:00:09 -0800

    working profile page

--- client/src/components/Metrics/Metrics.js
+++ client/src/components/Metrics/Metrics.js
@@ -1,18 +1,50 @@
 import React, { useEffect, useState } from "react";
+import Members from "../../pages/Members"
 
 import Chart from "../Charts/Chart"
 
-function Metrics() {
+function Metrics(props) {
 
+    function filterArrays(SpecificArray) {
+        var work = 0
+        var family = 0
+        var friends = 0
+        var mental = 0
+        var other = 0
+        for (let i = 0; i < SpecificArray.length; i++) {
+            switch (SpecificArray[i].reason) {
+                case "Work":
+                    work++
+                    break;
+                case "Family":
+                    family++
+                    break;
+                case "Friends":
+                    friends++
+                    break;
+                case "Mental":
+                    mental++
+                    break;
+                case "Other":
+                    other++
+                    break;
+                default:
+                    break;
+            }
+        }
+        return [work, family, friends, mental, other]
+    }
+
+    console.log(filterArrays(props.goodPosts))
 
     return (
         <div>
             <div className="md:flex md:justify-around">
                 <div className="w-full md:w-1/2 m-2 mb-6">
-                    <Chart title="good day results" data={[65, 59, 80, 81, 56]} labels={['Work', 'Family', 'Friends',
+                    <Chart title="good day results" data={filterArrays(props.goodPosts)} labels={['Work', 'Family', 'Friends',
                         'Mental', 'Other']} /></div>
                 <div className="w-full md:w-1/2 m-2 mb-6">
-                    <Chart title="bad day results" data={[65, 59, 80, 81, 56]} labels={['Work', 'Family', 'Friends',
+                    <Chart title="bad day results" data={filterArrays(props.badPosts)} labels={['Work', 'Family', 'Friends',
                         'Mental', 'Other']} /></div>
             </div>
         </div>
--- client/src/pages/Members.js
+++ client/src/pages/Members.js
@@ -12,7 +12,7 @@ import API from "../utils/API"
 
 function Members() {
 
-  const [User, setUser] = useState({ userName: "", user_id: "", good_day_percentage: 0, good_post_array: [], bad_post_array: [] });
+  const [User, setUser] = useState({ userName: "", user_id: "", good_day_percentage: 0, totalPosts: 0, good_post_array: [], bad_post_array: [], currentGoodDayStreak: 0 });
   const [userDataRetrieved, setUserDataRetrieved] = useState(false);
   const [postDataRetrieved, setPostDataRetrieved] = useState(false);
 
@@ -33,7 +33,7 @@ function Members() {
       // console.log(User)
       getPosts()
     }
-      
+
   }, [userDataRetrieved])
 
 
@@ -49,6 +49,7 @@ function Members() {
   }
 
   function filterDayQuality(Post) {
+
     var good_array = []
     var bad_array = []
     for (let i = 0; i < Post.data.length; i++) {
@@ -58,11 +59,20 @@ function Members() {
         bad_array.push(Post.data[i])
       }
     }
-    setUser({ ...User, bad_post_array: bad_array, good_post_array: good_array });
+
+    var currentPosition = 0
+    var postArrayReverse = Post.data.reverse()
+    var currentStreak = 0
+    while (postArrayReverse[currentPosition].day_quality === "Good") {
+      currentStreak = currentStreak + 1
+      currentPosition++;
+    }
+
+    var numberOfPosts = Post.data.length
+    var goodDayPercent = Math.round((good_array.length / Post.data.length) * 100)
+
+    setUser({ ...User, bad_post_array: bad_array, good_post_array: good_array, good_day_percentage: goodDayPercent, totalPosts: numberOfPosts, currentGoodDayStreak: currentStreak });
     setPostDataRetrieved(true);
-    // console.log(User)
-    console.log(good_array)
-    console.log(bad_array)
   }
   // Matt Milici adds
 
@@ -80,46 +90,46 @@ function Members() {
           : !postDataRetrieved
             ? <div>Keep waiting</div>
             : <div className="App">
-            <div className="bg-lime1">
-              <div className="p-auto max-w-screen-xl mx-auto py-12 px-4 sm:py-16 sm:px-6 lg:px-8 lg:py-20">
-                <div className="max-w-4xl mx-auto text-center font-bold">
-                  <h2 className="text-3xl leading-9 text-white sm:text-4xl sm:leading-10">
-                    Welcome <span className="member-name">{User.userName}</span>
-                  </h2>
-                  <p className="text-lg text-white text-opacity-75 leading-6 font-medium">
-                    Hopefully you're having a <span className="text-white">great</span> day!
+              <div className="bg-lime1">
+                <div className="p-auto max-w-screen-xl mx-auto py-12 px-4 sm:py-16 sm:px-6 lg:px-8 lg:py-20">
+                  <div className="max-w-4xl mx-auto text-center font-bold">
+                    <h2 className="text-3xl leading-9 text-white sm:text-4xl sm:leading-10">
+                      Welcome <span className="member-name">{User.userName}</span>
+                    </h2>
+                    <p className="text-lg text-white text-opacity-75 leading-6 font-medium">
+                      Hopefully you're having a <span className="text-white">great</span> day!
               </p>
+                  </div>
+                  <dl className="mt-10 text-center sm:max-w-3xl sm:mx-auto sm:grid sm:grid-cols-3 sm:gap-8">
+                    <div className="flex flex-col">
+                      <dt className="order-2 mt-2 text-lg leading-6 font-medium text-white text-opacity-75">
+                        Total Posts
+                  </dt>
+                      <dd id="totalSubmissions" className="order-1 text-5xl leading-none font-extrabold text-white">
+                        {User.totalPosts}
+                      </dd>
+                    </div>
+                    <div className="flex flex-col mt-10 sm:mt-0">
+                      <dt className="order-2 mt-2 text-lg leading-6 font-medium text-white text-opacity-75">
+                        Good Day %
+                  </dt>
+                      <dd className="order-1 text-5xl leading-none font-extrabold text-white">
+                        <span id="totalGoodDayCalc">{User.good_day_percentage}</span>
+                      </dd>
+                    </div>
+                    <div className="flex flex-col mt-10 sm:mt-0">
+                      <dt className="order-2 mt-2 text-lg leading-6 font-medium text-white text-opacity-75">
+                        Current Streak
+                  </dt>
+                      <dd id="totalGoodDayStreak" className="order-1 text-5xl leading-none font-extrabold text-white">
+                        {User.currentGoodDayStreak}
+                      </dd>
+                    </div>
+                  </dl>
                 </div>
-                <dl className="mt-10 text-center sm:max-w-3xl sm:mx-auto sm:grid sm:grid-cols-3 sm:gap-8">
-                  <div className="flex flex-col">
-                    <dt className="order-2 mt-2 text-lg leading-6 font-medium text-white text-opacity-75">
-                      Total Posts
-                  </dt>
-                    <dd id="totalSubmissions" className="order-1 text-5xl leading-none font-extrabold text-white">
-                      0
-                  </dd>
-                  </div>
-                  <div className="flex flex-col mt-10 sm:mt-0">
-                    <dt className="order-2 mt-2 text-lg leading-6 font-medium text-white text-opacity-75">
-                      Good Day %
-                  </dt>
-                    <dd className="order-1 text-5xl leading-none font-extrabold text-white">
-                      <span id="totalGoodDayCalc">{User.good_day_percentage}</span>
-                    </dd>
-                  </div>
-                  <div className="flex flex-col mt-10 sm:mt-0">
-                    <dt className="order-2 mt-2 text-lg leading-6 font-medium text-white text-opacity-75">
-                      Current Streak
-                  </dt>
-                    <dd id="totalGoodDayStreak" className="order-1 text-5xl leading-none font-extrabold text-white">
-                      0
-                </dd>
-                  </div>
-                </dl>
               </div>
+              <Metrics goodPosts={User.good_post_array} badPosts={User.bad_post_array} />
             </div>
-            <Metrics />
-          </div>
 
       }
     </div>
