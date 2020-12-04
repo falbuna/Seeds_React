1b28d83dabc1076332959d662117e5868a2907a1
Francis Albuna <falbuna1@gmail.com>
2020-12-03 21:31:40 -0800

    Made the calendar a component. Styled the calendar.

---
+++ client/src/components/HistoryCalendar/index.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import Calendar from 'react-calendar';
+import "./style.css"
+
+function HistoryCalendar(){
+
+    const [value, setValue] = useState(new Date());
+
+    function onChange(nextValue) {
+        setValue(nextValue);
+      }
+
+      return (
+        <div>
+          <Calendar
+            onChange={onChange}
+            value={value}
+          />
+        </div>
+      )
+    }
+
+  export default HistoryCalendar;
--- client/src/pages/History.js
+++ client/src/pages/History.js
@@ -1,19 +1,10 @@
-import React, { useState } from 'react';
-import Calendar from 'react-calendar';
-import 'react-calendar/dist/Calendar.css';
+import React from 'react';
+import HistoryCalendar from '../components/HistoryCalendar'
 
 function History(){
-    const [value, setValue] = useState(new Date());
-
-    function onChange(nextValue) {
-        setValue(nextValue);
-      }
 
     return (
-        <Calendar
-            onChange={onChange}
-            value={value}
-        />
+        <HistoryCalendar />
     );
 }
 
