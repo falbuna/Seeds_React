571523023636f2577876af6685e64aba7ec3d424
Francis Albuna <falbuna1@gmail.com>
2020-12-08 22:04:15 -0800

    Got the events to work on the calendar.

--- client/src/components/HistoryCalendar/index.js
+++ client/src/components/HistoryCalendar/index.js
@@ -3,6 +3,7 @@ import { Calendar, momentLocalizer} from 'react-big-calendar';
 import UserContext from "../../utils/UserContext";
 import moment from 'moment';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
+import "./style.css";
 
 
 const localizer = momentLocalizer(moment);
@@ -26,41 +27,66 @@ function HistoryCalendar(){
     let cellStyle = React.cloneElement(Children.only(children), {
         style: {
             ...children.style,
-            backgroundColor: valueDay == myDate ? 'red' : '',
+            backgroundColor: valueDay === CURRENT_DATE ? 'lightgreen' : '',
         },
-    });
-    return cellStyle;
+    })
+  return cellStyle;
   }
 
+  const eventStyleGetter = () => {
+    var style = {
+      backgroundColor: 'red',
+      opacity: 0.8,
+      color: 'black',
+      height: '500px'
+    };
+    return {
+      style: style
+    }
+  }
+  
   console.log(userState.good_post_array);
   console.log(userState.bad_post_array);
-  const day = userState.good_post_array[0].createdAt;
-  // console.log(day)
-  const utcDate =  moment.utc(day).toDate();
-  const myDate = moment(utcDate).local().format("MM/DD/YYYY");
-  console.log(myDate)
-  console.log(CURRENT_DATE);
+  // const day = userState.bad_post_array[0].createdAt;
+  // const utcDate =  moment.utc(day).toDate();
+  // const dayOfWeek = moment(userState.bad_post_array[0].createdAt).local().format("MM/DD/YYYY");
+  // console.log(dayOfWeek)
 
   function sortBadDays(){
     let badDates;
     for (let i = 0; i < userState.bad_post_array.length; i++){
     badDates = moment(userState.bad_post_array[i].createdAt).local().format("MM/DD/YYYY");
-    console.log(badDates)
     badDayDates.push(badDates);
   }
   setBadDates(badDayDates)
 }
 
-console.log(badDayDates)
+function createEvents(post_array){
+  let badEvents = [];
+  post_array.map(bad_post => {
+    const currentDay = moment(bad_post.createdAt).local().format("MM/DD/YYYY");
+    const post = {
+      allDay: true,
+      end: currentDay,
+      start: currentDay,
+      // title: 'Bad day',
+    }
+    badEvents.push(post);
+  })
+  return badEvents;
+}
+
+console.log(createEvents(userState.bad_post_array))
 
       return (
-        <div style={{ height: 600 }}>
+        <div className="rbc-calendar">
         <Calendar
           localizer={localizer}
-          events={[]}
+          events={createEvents(userState.bad_post_array)}
           views={["month"]}
-          startAccessor="startDate"
-          endAccessor="endDate"
+          startAccessor="start"
+          endAccessor="end"
+          eventPropGetter={eventStyleGetter}
           components={{
             dateCellWrapper: ColoredDateCellWrapper
           }}
